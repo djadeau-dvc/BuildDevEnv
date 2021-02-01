9d7cfb198092b57e45dd106f0f6f1f8068c71a09
Damien JADEAU <damien.jadeau@datavalue-consulting.com>
2021-02-01 15:49:11 +0100

    bêta version of builddevenv.

--- python/test/test_builddevenv.py
+++ python/test/test_builddevenv.py
@@ -6,6 +6,18 @@ import os
 class TestBuildDevEnv:
 
     def test_buildenv_ok(self):
+        """
+        Tests the builddevenv package based on the following scenario:
+        1. Creates the networks and the containers
+        2. Stops the containers
+        3. Starts the containers previously stopped
+        4. Remove the containers and the networks previously created.
+        """
         client = docker.from_env()
-        builder = Builder(os.path.join(__file__, "test/test_data/env_var.yaml"))
+        builder = Builder(os.path.join(os.path.dirname(__file__), "test_data\\env_var.yaml"))
+
+        builder.run()
+        builder.stop()
+        builder.start()
+        builder.delete()
         assert 1 == 1
